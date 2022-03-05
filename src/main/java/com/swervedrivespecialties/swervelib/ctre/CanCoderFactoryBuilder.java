51d1213f34f9d9a7a72b48ceb11ab3c63e7c0ebd
leanna <xcayote@gmail.com>
2022-03-05 14:17:44 -0800

    changes idk

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/CanCoderFactoryBuilder.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/CanCoderFactoryBuilder.java
@@ -23,7 +23,7 @@ public class CanCoderFactoryBuilder {
             config.magnetOffsetDegrees = Math.toDegrees(configuration.getOffset());
             config.sensorDirection = direction == Direction.CLOCKWISE;
 
-            CANCoder encoder = new CANCoder(configuration.getId(), "Canvore");
+            CANCoder encoder = new CANCoder(configuration.getId());
             encoder.configAllSettings(config, 250);
 
             encoder.setStatusFramePeriod(CANCoderStatusFrame.SensorData, periodMilliseconds, 250);
